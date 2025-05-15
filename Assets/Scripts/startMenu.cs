c284d264b032a639e3d41244ff9e3f78c1f6b111
XorsDomain <110067216+XorsDomain@users.noreply.github.com>
2025-05-14 21:49:13 -0400

    Implemented start menu logic

--- Assets/Scripts/startMenu.cs
+++ Assets/Scripts/startMenu.cs
@@ -1,12 +1,27 @@
 using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using UnityEngine.SceneManagement;
+
 
 public class StartMenu : MonoBehaviour
 {
+    [SerializeField] GameObject optionsUI;
+    [SerializeField] GameObject startMenu;
+
+    private string sceneName;
+
     public void StartGame()
     {
         // Load the game scene
+        sceneName = "SampleScene";
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void GoToOptions()
+    {
+        startMenu.SetActive(false);
+        optionsUI.SetActive(true);
     }
 
     public void QuitGame()
@@ -14,4 +29,6 @@ public class StartMenu : MonoBehaviour
         Application.Quit();
     }
 }
+
+
 
